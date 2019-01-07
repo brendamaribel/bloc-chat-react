bf8d28bbf51901fcda52cc9c43971820cb2a085d
Brenda Velasquez <brenda.velasquez99@gmail.com>
2019-01-06 22:42:16 -0700

    Updates

---
+++ precache-manifest.c1a63f29fca0fc22467a37b335d2b722.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "06b8f89ef212c0f9d4d3",
+    "url": "/bloc-chat-react/static/js/runtime~main.06b8f89e.js"
+  },
+  {
+    "revision": "2f2d092482fd72d11ad4",
+    "url": "/bloc-chat-react/static/js/main.2f2d0924.chunk.js"
+  },
+  {
+    "revision": "97406e8da5af83ee435f",
+    "url": "/bloc-chat-react/static/js/1.97406e8d.chunk.js"
+  },
+  {
+    "revision": "2f2d092482fd72d11ad4",
+    "url": "/bloc-chat-react/static/css/main.eb17d667.chunk.css"
+  },
+  {
+    "revision": "3e9ed9addbef33e1dacfe8ee454c2c55",
+    "url": "/bloc-chat-react/index.html"
+  }
+];
